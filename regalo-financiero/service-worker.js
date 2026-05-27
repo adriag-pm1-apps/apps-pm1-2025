abe331f66924932466edd415dd55bc038c06aa33
adriag-pm1-apps <adri.ag@preparatoriavasconcelos.edu.mx>
2026-05-26 23:12:56 -0700

    Add files via upload

--- regalo-financiero/service-worker.js
+++ regalo-financiero/service-worker.js
@@ -1,37 +1,22 @@
-const CACHE_NAME = 'regalo-financiero-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
+const CACHE_NAME = 'regalo-financiero-v6-puentes-claros-20260527';
+const ASSETS = ['./','./index.html','./manifest.json','./service-worker.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE_NAME ? null : caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
+    fetch(event.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return resp;
+    }).catch(() => caches.match(event.request).then(resp => resp || caches.match('./index.html')))
   );
 });
